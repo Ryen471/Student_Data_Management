e88209ed14d790a3200a3162a3d3a578a38ae88e
Aryan Rawal <aryanrawal107@gmail.com>
2026-02-14 12:00:32 +0530

    commit

--- JS/index.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById("admin").addEventListener("click", function () {
-    window.location.href = "/pages/adminlog.html";
-});
-
-document.getElementById("student").addEventListener("click", function () {
-    window.location.href = "/pages/studentlog.html";
-});
